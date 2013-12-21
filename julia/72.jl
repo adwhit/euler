a2d0b279db19fcce1b08299b2d38588374160a05
Alex Whitney <aw1209@ic.ac.uk>
2013-12-21 10:16:26 +0000

    offline mode

--- julia/72.jl
+++ julia/72.jl
@@ -6,12 +6,14 @@ function ctfracs(N::Int)
     for n=2:N
         ct = n-1
         facts = unique(primefacts(n, primes))
+        red = reduce(*, facts)
+        rep = div(facts,red)
         for i in facts
             ct -= div(n,i) -1
-            ct += i -1
+            ct += div(red,n) - 1
         end
         tot += ct
-        println(n, " ", tot)
+        println(n, " ", ct, " ",tot)
     end
     return tot
 end
