c07ae246b088d6da30682d9d083e516735c75238
Alex Whitney <aw1209@ic.ac.uk>
2014-01-14 13:26:43 +0000

    90,91, tidy up

--- rust/90.rs
+++ rust/90.rs
@@ -1,4 +1,5 @@
 use std::hashmap::HashSet;
+use std::vec::append;
 
 fn choose(n:uint, c:uint) -> uint{
     let mut r = 1;
@@ -37,23 +38,7 @@ fn combination(c:&mut [uint],n:uint,p:uint,x:uint){
     for i in range(0,p) {
         c[i] -= 1
     }
-}
-
-fn counter(c:&mut [uint]) {
-    let mut l = c.len();
-    for i in range(0, l) {
-        if c[l-1-i] >= 7 {
-            c[l-1-i] = 0
-        } else {
-            c[l-1-i] += 1;
-            for j in range(1,l) {
-                if c[j] < c[j-1] {
-                    c[j] = c[j-1]
-                }
-            }
-            return
-        }
-    }
+    if c[5] == 9 { c[5] = 6 }
 }
 
 fn contains(c:&[uint],n:uint) -> bool {
@@ -65,32 +50,37 @@ fn contains(c:&[uint],n:uint) -> bool {
     false
 }
 
-fn legal(c:&[uint],d:&[uint]) -> bool {
-    let mut set = HashSet::new();
-    for n in c.iter().chain(d.iter()) { set.insert(n); }
-    if set.len() < 8 {return false}
+fn checker(c:&[uint], d:&[uint], i:uint) -> int {
+    let inc = contains(c, i);
+    let ind = contains(d, i);
+    if inc && ind { 
+        return 0
+    } else if !inc && !ind {
+        return -1
+    } else if inc {
+        return 1
+    } else {
+        return 2
+    }
+}
 
+fn legal(c:&[uint],d:&[uint]) -> bool {
     let mut ind:~[uint] = ~[];
     let mut inc:~[uint] = ~[];
-    if contains(c, 0) &! contains(d, 0) { ind.push(1); ind.push(4); ind.push(6); }
-    if contains(d, 0) &! contains(c, 0) { inc.push(1); inc.push(4); inc.push(6); }
-    if contains(c, 1) &! contains(d, 1){ ind.push(6); ind.push(7); }
-    if contains(d, 1) &! contains(c, 1){ inc.push(6); inc.push(7); }
-    if contains(c, 2) &! contains(d, 2){ ind.push(5); }
-    if contains(d, 2) &! contains(c, 2){ inc.push(5); }
-    if contains(c, 3) &! contains(d, 3){ ind.push(6); }
-    if contains(d, 3) &! contains(c, 3){ inc.push(6); }
-    if contains(c, 4) &! contains(d, 4){ ind.push(0); ind.push(6); }
-    if contains(d, 4) &! contains(c, 4){ inc.push(0); inc.push(6); }
-    if contains(c, 5) &! contains(d, 5){ ind.push(2); }
-    if contains(d, 5) &! contains(c, 5){ inc.push(2); }
-    if contains(c, 6) &! contains(d, 6){ ind.push(1); ind.push(3); ind.push(4); ind.push(0) }
-    if contains(d, 6) &! contains(c, 6){ inc.push(1); inc.push(3); inc.push(4); inc.push(0) }
-    if contains(c, 7) &! contains(d, 7){ ind.push(1); }
-    if contains(d, 7) &! contains(c, 7){ inc.push(1); }
-    //println!("{:?} {:?}", ind, inc);
-    if ind.len() > 6 { return false }
-    if inc.len() > 6 { return false }
+    let mut flag:int;
+    let reqs:~[~[uint]] = ~[~[1,4,6],~[6,8],~[5],~[6],~[0,6],~[2],~[3,4,0],~[],~[1]];
+    for i in range(0,9u) {
+        if i != 7 {
+            flag = checker(c, d, i);
+            if flag == -1 {
+                return false
+            } else if flag == 1 {
+                ind.push_all(reqs[i])
+            } else if flag == 2 {
+                inc.push_all(reqs[i])
+            }
+        }
+    }
     for &k in ind.iter() {
         if !contains(d, k) {
             return false
@@ -105,21 +95,19 @@ fn legal(c:&[uint],d:&[uint]) -> bool {
 }
 
 fn main() {
-    let rn = choose(8,6);
+    let rn = choose(10,6);
     let mut c = [0u, 0u,0u,0u,0u,0u];
     let mut d = [0u, 0u,0u,0u,0u,0u];
     let mut ct = 0;
     for i in range(0, rn) {
-        combination(c, 8, 6, i+1);
-        //counter(c);
+        combination(c, 10, 6, i+1);
         for j in range(i,rn) {
-            combination(d, 8, 6, j+1);
-            //counter(d);
-            //println!("{} {:?},{:?}", ct,c,d);
+            combination(d, 10, 6, j+1);
             if legal(c,d){
                 ct += 1;
-                println!("{} {:?},{:?}", ct,c,d);
+                //println!("{} {:?},{:?}", ct,c,d);
             }
         }
     }
+    println!("{}", ct);
 }
---
+++ rust/91.rs
@@ -0,0 +1,25 @@
+static MX:int = 51;
+
+fn main () {
+    let mut ct = 0;
+    for x1 in range(0, MX) {
+        for x2 in range(x1, MX) {
+            for y1 in range(0, MX) {
+                for y2 in range(0, MX) {
+                    if (x1 == 0 && y1 == 0) || (x2 == 0 && y2 == 0) || (x1 == x2 && y1 == y2) {
+                        continue
+                    }
+                    if (x1 ==x2) && (y1 < y2) {
+                        continue
+                    }
+                    if x1*x2 + y1*y2 == 0 ||
+                       x1*(x2-x1) + y1*(y2-y1) == 0 ||
+                       x2*(x2-x1) + y2*(y2-y1) == 0 {
+                       ct += 1;
+                    }
+                }
+            }
+        }
+    }
+    println!("{}", ct)
+}
